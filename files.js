8d6ff87a035d4ec72562ee0b2e74e033aecb8010
Alex Berdea <berdea.alex@yahoo.com>
2023-06-16 17:17:53 +0100

    2nd pipe

--- files.js
+++ files.js
@@ -24,6 +24,32 @@ const fs = require('fs')
 
 
 //directories
+if(!fs.existsSync('./assets')){
+    fs.mkdir('./assets', (err)=>{
+        if (err){
+            console.log(err)
+        }
+        console.log('folder created')
+    })
+}else{
+    fs.rmdir('./assets', (err)=>{
+        if(err){
+            console.log(err)
+        }
+        console.log('folder deleted')
+    })
+}
+
+
 
 
 //deleting
+
+if(fs.existsSync('./docs/deleteme.txt')){
+    fs.unlink('./docs/deleteme.txt', (err)=>{
+        if(err){
+            console.log(err)
+        }
+        console.log('file deleted')
+    })
+}
