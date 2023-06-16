8a276bc2daeb5844b1fa195bc0d5a9877610a27c
Alex Berdea <berdea.alex@yahoo.com>
2023-06-16 15:28:39 +0100

    1st commit

---
+++ files.js
@@ -0,0 +1,29 @@
+const { error } = require('console')
+const fs = require('fs')
+
+//reading
+
+// fs.readFile('./docs/blog12.txt', (err, data)=>{
+//     if (err){
+//         console.log(err)
+//     }
+//     console.log(data.toString())
+// })
+
+// console.log('last line')
+
+//writing
+// fs.writeFile('./docs/blog1.txt', 'hello, world', () => {
+//     console.log('file was written')
+// })
+
+// fs.writeFile('./docs/blog2.txt', 'hello, again', () => {
+//     console.log('file was written')
+// })
+
+
+
+//directories
+
+
+//deleting
